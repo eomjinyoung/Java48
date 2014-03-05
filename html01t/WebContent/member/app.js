c2ffcf0d3f8bd72945f97df1817eae457f37b2c3
J <jinyoung.eom@gmail.com>
2014-03-05 15:36:10 +0900

    .

--- html01t/WebContent/member/app.js
+++ html01t/WebContent/member/app.js
@@ -4,14 +4,29 @@ window.onload = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       //var obj = eval('(' + xhr.responseText + ')');
       var members = JSON.parse(xhr.responseText).jsonResult.data;
-      for (var i in members) {
-    	//1.tr 태그를 생성하라.
-    	//2.td 태그를 생성하라. -> td 태그의 콘텐츠를 설정하라. 
-    	//3.td 태그를 tr에 붙여라 (2 ~ 3을 네번 반복: 번호,이름,이메일,전화)
-    	//2.tr 태그를 table 태그에 붙인다.
-      }
+      var memberTable = document.getElementById("memberTable");
+      var tr = null;
+      members.forEach(function(member){
+    	tr = document.createElement('tr');
+    	[member.no,member.name,member.email,member.tel].forEach(function(value){
+      	  td = document.createElement('td');
+  	      td.innerHTML = value;
+  	      tr.appendChild(td);
+      	});
+      	memberTable.appendChild(tr);
+      });
     }	  
   };
   xhr.open('GET', 'http://localhost:8080/web02/member/ajax/list.do', true);
   xhr.send(null);
 };
+
+
+
+
+
+
+
+
+
+
