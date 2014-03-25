aeb6b32c25826bf7a410e203f9bdcc99351b1725
jinyoung <jinyoung.eom@gmail.com>
2014-03-25 09:40:57 +0900

    .

--- java01t/src/step01/exam04/step06/StudentMgtSystem.java
+++ java01t/src/step01/exam04/step06/StudentMgtSystem.java
@@ -51,10 +51,52 @@ public class StudentMgtSystem {
 	public static void executeList() {
 		// ArrayList로부터 꺼낸다.
 		// for(변수 : 배열 또는 항목의 타입이 지정된 Collection 구현체)
+		int i = 0;
 		for (StudentScore score : scoreList) {
-			System.out.println(score);
+			System.out.println(i++ + " " + score);
 		}
 	}
+	
+	private static void executeDelete(int no) {
+	  if (no >= 0 && no < scoreList.size()) {
+	  	scoreList.remove(no);
+	  	System.out.println("삭제하였습니다.");
+	  } else {
+	  	System.out.println("유효하지 않은 번호입니다.");
+	  }
+  }
+	
+	private static void executeUpdate(int no) {
+		if (no >= 0 && no < scoreList.size()) {
+			StudentScore score = scoreList.get(no);
+			System.out.print("이름(" + score.name + "):");
+			String input = scanner.nextLine();
+			StudentScore temp = new StudentScore(input);
+			
+			System.out.print("국어(" + score.kor + "):");
+			input = scanner.nextLine();
+			temp.kor = Integer.parseInt(input);
+			
+			System.out.print("영어(" + score.eng + "):");
+			input = scanner.nextLine();
+			temp.eng = Integer.parseInt(input);
+			
+			System.out.print("수학(" + score.math + "):");
+			input = scanner.nextLine();
+			temp.math = Integer.parseInt(input);
+			
+			System.out.print("변경하시겠습니까?(y/n)");
+			input = scanner.nextLine();
+			if ("y".equals(input.toLowerCase())) {
+				scoreList.set(no, temp);
+				System.out.println("변경되었습니다.");
+			} else {
+				System.out.println("변경 취소하였습니다.");
+			}
+		} else {
+			System.out.println("유효하지 않은 번호입니다.");
+		}
+  }
 	
 	public static void main(String[] args) {
 		do {
@@ -64,6 +106,10 @@ public class StudentMgtSystem {
 				executeAdd(values[1]);
 			} else if ("list".equals(values[0])) {
 				executeList();
+			} else if ("delete".equals(values[0])) {
+				executeDelete(Integer.parseInt(values[1]));
+			} else if ("update".equals(values[0])) {
+				executeUpdate(Integer.parseInt(values[1]));
 			} else if ("quit".equals(values[0])) {
 				break;
 			} else {
@@ -74,6 +120,8 @@ public class StudentMgtSystem {
 
 		scanner.close();
 	}
+
+
 
 
 }
