8ab3c8c1eb6b111f02eacb018ee0c946d7c88e72
jinyoung <jinyoung.eom@gmail.com>
2014-03-28 16:34:17 +0900

    .

--- java01t/src/basic/exam05/step04/ScorePanel.java
+++ java01t/src/basic/exam05/step04/ScorePanel.java
@@ -1,15 +1,35 @@
 package basic.exam05.step04;
 
+import java.awt.Button;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @SuppressWarnings("serial")
-public class ScorePanel extends Panel {
+public class ScorePanel extends Panel implements ActionListener{
 	StudentMgtSystem parent;
 	Label title = new Label("점수관리");
+	Button btnBack = new Button("뒤로");
 	
 	public ScorePanel(StudentMgtSystem parent) {
 		this.parent = parent; // 부모 윈도우의 주소를 보관
 		add(title);
+		btnBack.addActionListener(this);
+		add(btnBack);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		parent.changePanel(StudentMgtSystem.MENU_PANEL);
 	}
 }
+
+
+
+
+
+
+
+
+
--- java01t/src/basic/exam05/step04/StudentPanel.java
+++ java01t/src/basic/exam05/step04/StudentPanel.java
@@ -1,15 +1,34 @@
 package basic.exam05.step04;
 
+import java.awt.Button;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 @SuppressWarnings("serial")
-public class StudentPanel extends Panel {
+public class StudentPanel extends Panel implements ActionListener {
 	StudentMgtSystem parent;
 	Label title = new Label("학생관리");
-	
+	Button btnBack = new Button("뒤로");
+
 	public StudentPanel(StudentMgtSystem parent) {
 		this.parent = parent; // 부모 윈도우의 주소를 보관
 		add(title);
+		btnBack.addActionListener(this);
+		add(btnBack);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		parent.changePanel(StudentMgtSystem.MENU_PANEL);
 	}
 }
+
+
+
+
+
+
+
+
