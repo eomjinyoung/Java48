27f345220f0d42fd8be2aed1b705358ec72c78eb
jinyoung <jinyoung.eom@gmail.com>
2014-04-08 15:57:33 +0900

    .

--- java01t/src/basic/exam06/jdbc/up1/ListTest.java
+++ java01t/src/basic/exam06/jdbc/up1/ListTest.java
@@ -2,10 +2,13 @@ package basic.exam06.jdbc.up1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
-/* 
+/* 페이징 처리
+ * - 
  */
 public class ListTest {
 
@@ -17,17 +20,38 @@ public class ListTest {
 				"study", 
 				"study"); 
 		
-		Statement stmt = con.createStatement();
+		/*
+	 	select sno, title, dest 
+		from SE_SUBJS 
+		order by sno desc
+		limit 페이지 시작 인덱스, 페이지당 출력 개수
+	 */
+		PreparedStatement stmt = con.prepareStatement(
+				"select SNO, TITLE, DEST from SE_SUBJS"
+						+ " order by SNO desc"
+						+ " limit ?, ?");
+		Scanner scanner = new Scanner(System.in);
+		ResultSet rs = null;
+		int pageNo = 1, pageSize = 10;
+		do {
+			stmt.setInt(1, (pageNo - 1) * pageSize);
+			stmt.setInt(2, pageSize);
+			
+			rs = stmt.executeQuery();
+			
+			System.out.println("[" + pageNo + "]----------------------------");
+			while(rs.next()) {
+				System.out.print(rs.getInt("SNO") + ",");
+				System.out.print(rs.getString("TITLE") + ",");
+				System.out.println(rs.getString("DEST"));
+			}
+			rs.close();
+			
+			pageNo = Integer.parseInt(scanner.nextLine());
+			
+		} while(pageNo > 0);
 		
-		ResultSet rs = stmt.executeQuery("select SNO, TITLE, DEST from SE_SUBJS");
 		
-		while(rs.next()) {
-			System.out.print(rs.getInt("SNO") + ",");
-			System.out.print(rs.getString("TITLE") + ",");
-			System.out.println(rs.getString("DEST"));
-		}
-		
-		rs.close();
 		stmt.close();
 		con.close(); 
 	}
