d12300b4df27fe84bb85140799c33252f737262c
jinyoung <jinyoung.eom@gmail.com>
2014-04-08 16:19:19 +0900

    .

---
+++ java01t/src/basic/exam06/jdbc/ex/CourseMgt2.java
@@ -0,0 +1,43 @@
+package basic.exam06.jdbc.ex;
+
+public class CourseMgt2 {
+
+	public static void insert() {
+		
+	}
+	
+	public static void list() {
+		
+	}
+	
+	public static void update() {
+		
+	}
+	
+	public static void delete() {
+		
+	}
+	
+	public static void main(String[] args) {
+		//insert();
+		//list();
+		//update();
+		//delete();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
