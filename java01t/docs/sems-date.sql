cba89a938406cbe20a273747f0bd40a414273d00
jinyoung <jinyoung.eom@gmail.com>
2014-04-10 15:18:13 +0900

    .

---
+++ java01t/docs/sems-date.sql
@@ -0,0 +1,14 @@
+insert into SE_USERS(EMAIL,PWD,NAME,TEL)
+  values('hong@test.com', '1111', '홍길동', '1111-1111');
+insert into SE_USERS(EMAIL,PWD,NAME,TEL)
+  values('leem@test.com', '1111', '임꺽정', '1111-2222');
+insert into SE_USERS(EMAIL,PWD,NAME,TEL)
+  values('jang@test.com', '1111', '장보고', '1111-3333');
+insert into SE_USERS(EMAIL,PWD,NAME,TEL)
+  values('kang@test.com', '1111', '강감찬', '1111-4444');
+insert into SE_USERS(EMAIL,PWD,NAME,TEL)
+  values('kim@test.com', '1111', '김유신', '1111-5555');
+  
+  
+insert into SE_STUDS(UNO,COMP,POS,STAT,LAST_SCH,ACCT)
+  values(6,'비트나라','부장',1,'비트대학교','111-11-11111');  
