555c7042f1cd42aa4383572223deaf30b7845ea4
jinyoung <jinyoung.eom@gmail.com>
2014-04-14 16:25:42 +0900

    .

--- java01t/src/basic/exam06/episode/up4/Collection5.java
+++ java01t/src/basic/exam06/episode/up4/Collection5.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 // - ArrayList를 상속 받으면서 AbstractCollection을 상속 받을 수 없다.
 public class Collection5 
 		extends ArrayList<Object> /*, AbstractCollection*/ 
-		implements Collection {
+		/*implements Collection*/ {
 
 	// 문제 발생!
 	// ArrayList로부터 상속 받은 boolean add(Object value)와 
 	// Collection의 void add(Object value)가 시그너처가 같지 않다. 
 	//  => 컴파일 오류 발생 
 	// 해결책: Collection6 참조!
-	@Override
+	//@Override
   public void insert(int index, Object value) {
   }
 	
