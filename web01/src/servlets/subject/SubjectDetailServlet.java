ae4b5fc34143007dbb7ac91f8b18a06693647ea5
jinyoung <jinyoung.eom@gmail.com>
2014-04-15 11:59:15 +0900

    .

---
+++ web01/src/servlets/subject/SubjectDetailServlet.java
@@ -0,0 +1,79 @@
+package servlets.subject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.DBConnectionPool;
+import vo.SubjectVo;
+import dao.MysqlSubjectDao;
+
+@WebServlet("/subject/detail.bit")
+@SuppressWarnings("serial")
+public class SubjectDetailServlet extends HttpServlet {
+	@Override
+	protected void doGet(
+			HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>과목상세정보</title></head><body>");
+		try {
+			out.println("<h1>과목 상세정보</h1>");
+			
+			DBConnectionPool dbConnectionPool = new DBConnectionPool();
+			MysqlSubjectDao dao = new MysqlSubjectDao();
+			dao.setDBConnectionPool(dbConnectionPool);
+			
+			int no = Integer.parseInt(request.getParameter("no")); 
+			
+			SubjectVo subject = dao.detail(no);
+			
+			out.println("<table border='1'>");
+			out.println("<tr>");
+			out.println("	<th>번호</th>");
+			out.println("	<td>" + subject.getNo() + "</td>");
+			out.println("</tr>");
+			
+			out.println("<tr>");
+			out.println("	<th>과목명</th>");
+			out.println("	<td>" + subject.getTitle() + "</td>");
+			out.println("</tr>");
+			
+			out.println("<tr>");
+			out.println("	<th>내용</th>");
+			out.println(" <td><textarea rows='5' cols='60'>"
+					+ subject.getDescription()
+					+ "</textarea></td>");
+			out.println("</tr>");
+			
+			out.println("</table>");
+		} catch (Throwable e) {
+			out.println("오류 발생 했음!");
+			e.printStackTrace();
+		}
+		out.println("</body></html>");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ web01/src/servlets/subject/SubjectInsertServlet.java
@@ -0,0 +1,70 @@
+package servlets.subject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.DBConnectionPool;
+import vo.SubjectVo;
+import dao.MysqlSubjectDao;
+
+@WebServlet("/subject/insert.bit")
+@SuppressWarnings("serial")
+public class SubjectInsertServlet extends HttpServlet {
+	@Override
+	protected void doGet(
+			HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+		// POST 요청의 값에 대해 적용. GET 요청의 값은?
+		// - 서블릿 컨테이너의 안내에 따라 설정한다.
+		// - getParameter()를 호출하기 전에 실행해야 한다.
+		//   단, 한번이라도 getParameter()를 호출했다면 적용안됨. 
+		request.setCharacterEncoding("UTF-8");
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>과목등록</title></head><body>");
+		
+		try {
+			out.println("<h1>과목 등록 결과</h1>");
+			
+			DBConnectionPool dbConnectionPool = new DBConnectionPool();
+			MysqlSubjectDao dao = new MysqlSubjectDao();
+			dao.setDBConnectionPool(dbConnectionPool);
+			
+			SubjectVo vo = new SubjectVo();
+			vo.setTitle(request.getParameter("title"));
+			vo.setDescription(request.getParameter("description"));
+			
+			dao.insert(vo);
+			
+			out.println("등록 성공!");
+			
+		} catch (Throwable e) {
+			out.println("오류 발생 했음!");
+			e.printStackTrace();
+		}
+		out.println("</body></html>");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
