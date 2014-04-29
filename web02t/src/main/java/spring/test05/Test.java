8ec638ebb5aafa9d4ed38efff17bb1d366e66624
jinyoung <jinyoung.eom@gmail.com>
2014-04-29 12:11:21 +0900

    .

---
+++ web02t/src/main/java/spring/test05/Test.java
@@ -0,0 +1,29 @@
+package spring.test05;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext beanContainer = 
+				new ClassPathXmlApplicationContext("spring/test04/beans.xml");
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
