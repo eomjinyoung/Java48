8b096c8dde872a3d4c29d65939cce00b8ff3f5d9
jinyoung <jinyoung.eom@gmail.com>
2014-04-29 16:36:41 +0900

    .

--- web02t/src/main/java/spring/test09/Test.java
+++ web02t/src/main/java/spring/test09/Test.java
@@ -6,14 +6,22 @@ public class Test {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext beanContainer = 
-				new ClassPathXmlApplicationContext("spring/test08/beans.xml");
+				new ClassPathXmlApplicationContext("spring/test09/beans.xml");
 		
 		Tire t1 = (Tire) beanContainer.getBean("tire1");
 		System.out.println(t1);
 		
-		System.out.println("--------------------------------");
-		Car c1 = (Car) beanContainer.getBean("car1");
-		System.out.println(c1);
+		Tire t2 = (Tire) beanContainer.getBean("tire2");
+		System.out.println(t2);
+		
+		Tire t3 = (Tire) beanContainer.getBean("tire3");
+		System.out.println(t3);
+		
+		Tire t4 = (Tire) beanContainer.getBean("tire4");
+		System.out.println(t4);
+		
+		Tire t5 = (Tire) beanContainer.getBean("tire5");
+		System.out.println(t5);
 		
 	}
 }
