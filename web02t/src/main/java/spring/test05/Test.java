d6cfa0572eb4d5cf8dccfdcfcb4be0f00ecede02
jinyoung <jinyoung.eom@gmail.com>
2014-04-29 12:46:50 +0900

    .

--- web02t/src/main/java/spring/test05/Test.java
+++ web02t/src/main/java/spring/test05/Test.java
@@ -6,7 +6,32 @@ public class Test {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext beanContainer = 
-				new ClassPathXmlApplicationContext("spring/test04/beans.xml");
+				new ClassPathXmlApplicationContext("spring/test05/beans.xml");
+		
+		Engine e1 = (Engine) beanContainer.getBean("engine1");
+		Car c1 = (Car) beanContainer.getBean("car1");
+		Car c2 = (Car) beanContainer.getBean("car2");
+		Car c3 = (Car) beanContainer.getBean("car3");
+		Car c4 = (Car) beanContainer.getBean("car4");
+		
+		System.out.println(e1);
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		
+		if (c1.getEngine() == c2.getEngine()) {
+			System.out.println("c1.engine == c2.engine");
+		}
+		if (c2.getEngine() == c3.getEngine()) {
+			System.out.println("c2.engine == c3.engine");
+		}
+		if (c3.getEngine() == c4.getEngine()) {
+			System.out.println("c3.engine == c4.engine");
+		}
+		if (c4.getEngine() == e1) {
+			System.out.println("c4.engine == e1");
+		}
 		
 	}
 }
