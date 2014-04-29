3839e2507010bb7c5836061a498e026ea7532bd7
jinyoung <jinyoung.eom@gmail.com>
2014-04-29 16:44:42 +0900

    .

---
+++ web02t/src/main/java/spring/test10/Test.java
@@ -0,0 +1,35 @@
+package spring.test10;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext beanContainer = 
+				new ClassPathXmlApplicationContext("spring/test10/beans.xml");
+		
+		Tire t1 = (Tire) beanContainer.getBean("tire1");
+		System.out.println(t1);
+		
+		Tire t2 = (Tire) beanContainer.getBean("tire2");
+		System.out.println(t2);
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
