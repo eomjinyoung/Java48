0e1e0cca6d552ec5d62cfc0c806769c073d7a2bf
jinyoung <jinyoung.eom@gmail.com>
2014-05-01 16:00:43 +0900

    .

--- web02t/src/main/java/sems/dao/MysqlSubjectDao.java
+++ web02t/src/main/java/sems/dao/MysqlSubjectDao.java
@@ -1,13 +1,10 @@
 package sems.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import javax.sql.DataSource;
-
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,122 +13,81 @@ import sems.vo.SubjectVo;
 @Component
 public class MysqlSubjectDao implements SubjectDao {
 	@Autowired
-	DataSource dataSource;
+	SqlSessionFactory sqlSessionFactory;
 
 	public void insert(SubjectVo subject) throws Throwable {
-		Connection con = null;
-		PreparedStatement stmt = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			con = dataSource.getConnection();
-			stmt = con.prepareStatement(
-					"insert SE_SUBJS(TITLE, DEST) values(?, ?)");
-			stmt.setString(1, subject.getTitle());
-			stmt.setString(2, subject.getDescription());
-			stmt.executeUpdate();
+			sqlSession.insert("sems.subject.insert", subject);
+			sqlSession.commit();
 		} catch (Throwable e) {
 			throw e;
+			
 		} finally { 
-			try {stmt.close();} catch (Throwable e2) {}
-			try {con.close();} catch (Throwable e2) {}
+			try {sqlSession.close();} catch (Throwable e2) {}
 		}
 	}
 	
 	public List<SubjectVo> list(int pageNo, int pageSize) 
 			throws Throwable {
-		Connection con = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			con = dataSource.getConnection();
-			stmt = con.prepareStatement(
-					"select SNO, TITLE from SE_SUBJS"
-							+ " order by SNO desc"
-							+ " limit ?, ?");
-			stmt.setInt(1, (pageNo - 1) * pageSize);
-			stmt.setInt(2, pageSize);
-			rs = stmt.executeQuery();
+			HashMap<String,Integer> params = new HashMap<String,Integer>();
+			params.put("startIndex", (pageNo - 1) * pageSize);
+			params.put("pageSize", pageSize);
+
+			return sqlSession.selectList("sems.subject.list", params);
 			
-			ArrayList<SubjectVo> list = new ArrayList<SubjectVo>();
-			while(rs.next()) {
-				list.add(new SubjectVo()
-													.setNo(rs.getInt("SNO"))
-													.setTitle(rs.getString("TITLE")));
-			}
-			return list;
 		} catch (Throwable e) {
 			throw e;
+			
 		} finally { 
-			try {rs.close();} catch (Throwable e2) {}
-			try {stmt.close();} catch (Throwable e2) {}
-			try {con.close();} catch (Throwable e2) {}
+			try {sqlSession.close();} catch (Throwable e2) {}
 		}
 	}
 	
 	public SubjectVo detail(int no) throws Throwable {
-		Connection con = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			con = dataSource.getConnection();
-			stmt = con.prepareStatement(
-					"select SNO, TITLE, DEST from SE_SUBJS"
-							+ " where SNO=?");
-			stmt.setInt(1, no);
-			rs = stmt.executeQuery();
-			
-			if (rs.next()) {
-				return new SubjectVo()
-										.setNo(rs.getInt("SNO"))
-										.setTitle(rs.getString("TITLE"))
-										.setDescription(rs.getString("DEST"));
-			} else {
+			SubjectVo vo = sqlSession.selectOne("sems.subject.detail", no);
+			if (vo == null) {
 				throw new Exception("해당 과목을 찾을 수 없습니다.");
 			}
+			return vo;
+			
 		} catch (Throwable e) {
 			throw e;
+			
 		} finally { 
-			try {rs.close();} catch (Throwable e2) {}
-			try {stmt.close();} catch (Throwable e2) {}
-			try {con.close();} catch (Throwable e2) {}
+			try {sqlSession.close();} catch (Throwable e2) {}
 		}
 	}
 	
 	public void update(SubjectVo subject) throws Throwable {
-		Connection con = null;
-		PreparedStatement stmt = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			con = dataSource.getConnection();
-			stmt = con.prepareStatement(
-					"update SE_SUBJS set"
-							+ " TITLE=?" 
-							+ ", DEST=?"
-							+ " where SNO=?");
-			stmt.setString(1, subject.getTitle());
-			stmt.setString(2, subject.getDescription());
-			stmt.setInt(3, subject.getNo());
-			stmt.executeUpdate();
+			sqlSession.update("sems.subject.update", subject);
+			sqlSession.commit();
+			
 		} catch (Throwable e) {
 			throw e;
+			
 		} finally { 
-			try {stmt.close();} catch (Throwable e2) {}
-			try {con.close();} catch (Throwable e2) {}
+			try {sqlSession.close();} catch (Throwable e2) {}
 		}
 	}
 	
 	public void delete(int no) throws Throwable {
-		Connection con = null;
-		PreparedStatement stmt = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			con = dataSource.getConnection();
-			stmt = con.prepareStatement(
-					"delete from SE_SUBJS where SNO=?"	);
-			stmt.setInt(1, no);
-			stmt.executeUpdate();
+			sqlSession.delete("sems.subject.delete", no);
+			sqlSession.commit();
+			
 		} catch (Throwable e) {
 			throw e;
+			
 		} finally { 
-			try {stmt.close();} catch (Throwable e2) {}
-			try {con.close();} catch (Throwable e2) {}
+			try {sqlSession.close();} catch (Throwable e2) {}
 		}
 	}
 }
