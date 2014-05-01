2ed6183b9776aaa8d246a4994552fa1b1c7e5fb1
jinyoung <jinyoung.eom@gmail.com>
2014-05-01 13:06:32 +0900

    .

--- web02t/src/main/java/mybatis/Test01.java
+++ web02t/src/main/java/mybatis/Test01.java
@@ -11,8 +11,42 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import sems.vo.SubjectVo;
 
 public class Test01 {
-
 	public static void main(String[] args) throws Exception {
+		String mybatisConfigFile = "mybatis/mybatis.xml";
+		InputStream configStream = 
+				Resources.getResourceAsStream(mybatisConfigFile);
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(configStream);
+		SqlSession sqlSession = factory.openSession();
+		
+		SubjectVo vo = sqlSession.selectOne("test.subject.detail", 87);
+		
+		System.out.format("%1$3d %2$s\n", vo.getNo(), vo.getTitle());
+		System.out.println(vo.getDescription());
+		
+		sqlSession.close();
+	}
+	
+	public static void main02(String[] args) throws Exception {
+		String mybatisConfigFile = "mybatis/mybatis.xml";
+		InputStream configStream = 
+				Resources.getResourceAsStream(mybatisConfigFile);
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(configStream);
+		SqlSession sqlSession = factory.openSession();
+		
+		SubjectVo vo = new SubjectVo()
+			.setTitle("aaaa2")
+			.setDescription("aaaaa2");
+		
+		sqlSession.insert("test.subject.insert", vo);
+		sqlSession.commit();
+		System.out.println("입력 완료!");
+		
+		sqlSession.close();
+	}
+	
+	public static void main01(String[] args) throws Exception {
 		String mybatisConfigFile = "mybatis/mybatis.xml";
 		InputStream configStream = 
 				Resources.getResourceAsStream(mybatisConfigFile);
